7b15d7aaa4a3b991b3cf5f773ab9bcb8215b0d63
korus <ruslan.kosarev@gmail.com>
2022-08-07 13:55:54 +0300

    update market.py

--- examples/market.py
+++ examples/market.py
@@ -10,16 +10,6 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 
-def generator(instruments):
-    for item in instruments().instruments:
-        yield ({
-            'ticker': item.ticker,
-            'figi': item.figi,
-            'type': instruments.__name__,
-            'name': item.name,
-        })
-
-
 def market():
     secrets = get_secrets()
     api_key = secrets.get_api_key('tinvest_api_key')
@@ -32,11 +22,11 @@ def market():
         futures = client.instruments.futures
 
         for instruments in (shares, bonds, etfs, currencies, futures):
-            df = pd.DataFrame(generator(instruments))
+            df = pd.DataFrame(instruments().instruments)
             df = df.set_index('ticker')
 
             print()
-            print(f'Market instruments for {instruments.__name__} (count {len(df)})')
+            print(instruments)
             print(df)
 
 
