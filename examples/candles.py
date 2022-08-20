a80b4d3918ef75485be4086a758e368bd01beb3b
korus <ruslan.kosarev@gmail.com>
2022-08-20 11:42:32 +0300

    function renamed

--- examples/candles.py
+++ examples/candles.py
@@ -1,12 +1,13 @@
 
 import pandas as pd
 from tinkoff.invest import CandleInterval, Client
+from tinkoff.invest.utils import quotation_to_decimal
 
 from examples.utils.secrets import get_secrets
 
 
-def price_value(x) -> float:
-    return x.units + x.nano/1000000000
+def quote_to_float(x) -> float:
+    return float(quotation_to_decimal(x))
 
 
 pd.set_option('display.max_rows', 50)
@@ -31,10 +32,10 @@ def candles():
         def generator():
             for candle in client.get_all_candles(figi=figi, from_=start, to=end, interval=interval):
                 yield {
-                    'open': price_value(candle.open),
-                    'high': price_value(candle.high),
-                    'low': price_value(candle.low),
-                    'close': price_value(candle.close),
+                    'open': quote_to_float(candle.open),
+                    'high': quote_to_float(candle.high),
+                    'low': quote_to_float(candle.low),
+                    'close': quote_to_float(candle.close),
                     'volume': candle.volume,
                     'time': candle.time,
                     'is_complete': candle.is_complete,
